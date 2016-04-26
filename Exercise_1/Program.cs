b54188e08e2d823c1935503d4858701c5ae38cc1
Andy Bhadai <abhadai@gmail.com>
2016-04-26 15:25:03 +0200

    Started exercise 1

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        interface INumber
+        {
+            void visit(INumberVisitor INumberVisitor);
+        }
+
+        interface INumberVisitor
+        {
+            void onMyInt(MyInt MyInt);
+            void onMyFloat(MyFloat MyFloat);
+        }
+
+        class MyInt : INumber
+        {
+            public MyInt()
+            {
+            }
+
+            public void visit(INumberVisitor INumberVisitor)
+            {
+                INumberVisitor.onMyInt(this);
+            }
+        }
+
+        class MyFloat : INumber
+        {
+            public MyFloat()
+            {
+            }
+
+            public void visit(INumberVisitor INumberVisitor)
+            {
+                INumberVisitor.onMyFloat(this);
+            }
+        }
+
+        class NumberVisitor : INumberVisitor
+        {
+            public void onMyFloat(MyFloat MyFloat)
+            {
+                Console.WriteLine("Found float!");
+            }
+
+            public void onMyInt(MyInt MyInt)
+            {
+                Console.WriteLine("Found int!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
