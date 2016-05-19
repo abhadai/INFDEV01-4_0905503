2518f433e8cbe662e1dcbbf78b20048a91b3098f
Andy Bhadai <abhadai@gmail.com>
2016-05-19 20:15:50 +0200

    Started exercise 3, still needs work

--- Exercise_3/Program.cs
+++ Exercise_3/Program.cs
@@ -8,6 +8,40 @@ namespace Exercise_3
 {
     class Program
     {
+        interface IOption<T>
+        {
+            U Visit<U>(IOptionVisitor<T, U> OptionVisitor);
+        }
+
+        interface IOptionVisitor<T, U>
+        {
+            U onSome<U>(T Value);
+            U onNone<U>();
+        }
+
+        class None<T> : IOption<T>
+        {
+            public U Visit<U>(IOptionVisitor<T, U> OptionVisitor)
+            {
+                return OptionVisitor.onNone();
+            }
+        }
+
+        class Some<T> : IOption<T>
+        {
+            T Value;
+
+            public Some(T value)
+            {
+                this.Value = value;
+            }
+
+            public U Visit<U>(IOptionVisitor<T, U> OptionVisitor)
+            {
+                return OptionVisitor.onSome(this.Value);
+            }
+        }
+
         static void Main(string[] args)
         {
         }
